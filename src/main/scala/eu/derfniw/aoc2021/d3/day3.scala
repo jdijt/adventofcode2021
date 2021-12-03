cf70548faf7bbff92e51dda28fb00ae5a77ffbf1
Jasper Dijt <jasper.dijt@lunatech.nl>
2021-12-03 09:39:21 +0100

    Day 3, working, but ROUGH, maybe try to make this nicer at some point.

--- src/main/scala/eu/derfniw/aoc2021/d3/day3.scala
+++ src/main/scala/eu/derfniw/aoc2021/d3/day3.scala
@@ -2,8 +2,75 @@ package eu.derfniw.aoc2021.d3
 
 import scala.io.Source;
 
-def exercise1(in: Source): Int = ???
-def exercise2(in: Source): Int = ???
+private def parseInput(in: Source) = {
+  in.getLines().map(_.map(_.toString.toInt).reverse).toSeq
+}
+
+private def bitCounts(binNums: Seq[Seq[Int]]): Seq[Int] =
+  binNums.foldLeft(List.fill(binNums.head.length)(0)) { (counts, nums) =>
+    counts.zip(nums).map((count, num) => count + num)
+  }
+
+def exercise1(in: Source): Int = {
+  val input       = parseInput(in)
+  val inputLength = input.length
+  val counts      = bitCounts(input)
+
+  val gamma = counts.zipWithIndex.map { (num, idx) =>
+    if (num > inputLength / 2) 1 * math.pow(2, idx).toInt
+    else 0
+  }.sum
+
+  val epsilon = counts.zipWithIndex.map { (num, idx) =>
+    if (num < inputLength / 2) 1 * math.pow(2, idx).toInt
+    else 0
+  }.sum
+
+  gamma * epsilon
+}
+
+def exercise2(in: Source): Int = {
+  val input  = parseInput(in)
+  val counts = bitCounts(input)
+
+  val ox = List
+    .range(0, input.head.length)
+    .reverse
+    .foldLeft((input, counts)) {
+      case ((i, c), _) if i.length == 1 =>
+        println(s"Found: ${i.head.reverse}")
+        (i, c)
+      case ((i, c), idx) =>
+        val mostCommon = if (c(idx) < i.length / 2f) 0 else 1
+        val newNums    = i.filter(_(idx) == mostCommon)
+        (newNums, bitCounts(newNums))
+    }
+    ._1
+    .head
+    .zipWithIndex
+    .map((n, i) => n * math.pow(2, i).toInt)
+    .sum
+
+  val co2 = List
+    .range(0, input.head.length)
+    .reverse
+    .foldLeft((input, counts)) {
+      case ((i, c), _) if i.length == 1 =>
+        println(s"Found: ${i.head.reverse}")
+        (i, c)
+      case ((i, c), idx) =>
+        val leastCommon = if (c(idx) < i.length / 2f) 1 else 0
+        val newNums     = i.filter(_(idx) == leastCommon)
+        (newNums, bitCounts(newNums))
+    }
+    ._1
+    .head
+    .zipWithIndex
+    .map((n, i) => n * math.pow(2, i).toInt)
+    .sum
+
+  ox * co2
+}
 
 private lazy val input: Source = Source.fromResource("exerciseInputs/input_d03.txt")
 
--- src/test/scala/eu/derfniw/aoc2021/d3/Day3Test.scala
+++ src/test/scala/eu/derfniw/aoc2021/d3/Day3Test.scala
@@ -5,17 +5,28 @@ import scala.io.Source
 class Day3Test extends munit.FunSuite {
 
   private val testInput =
-    """|""".stripMargin
+    """|00100
+       |11110
+       |10110
+       |10111
+       |10101
+       |01111
+       |00111
+       |11100
+       |10000
+       |11001
+       |00010
+       |01010""".stripMargin
 
   test("Part 1 is ok per example") {
     val result = exercise1(Source.fromString(testInput))
 
-    assertEquals(result, 7)
+    assertEquals(result, 198)
   }
 
   test("Part 2 is ok per example") {
     val result = exercise2(Source.fromString(testInput))
 
-    assertEquals(result, 5)
+    assertEquals(result, 230)
   }
 }
