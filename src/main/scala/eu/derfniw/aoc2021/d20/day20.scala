9953a9a5c7c98319ccef435bce399c360ae013db
Jasper Dijt <jasper.dijt@lunatech.nl>
2021-12-20 10:07:03 +0100

    D20 done.

--- src/main/scala/eu/derfniw/aoc2021/d20/day20.scala
+++ src/main/scala/eu/derfniw/aoc2021/d20/day20.scala
@@ -10,12 +10,12 @@ extension (s: Source)
     val algorithm = lines.next()
     // Skip line
     lines.next()
-    val grid = Grid(lines.toIndexedSeq)
+    val grid = Grid(lines.map(_.toIndexedSeq).toIndexedSeq)
     (algorithm, grid)
 
-class Grid(g: IndexedSeq[String], defaultChar: Char = '.'):
+class Grid(g: IndexedSeq[IndexedSeq[Char]], defaultChar: Char = '.'):
 
-  def getBitValue(xIndex: Int, yIndex: Int): Int =
+  private def getBitValue(xIndex: Int, yIndex: Int): Int =
     val neighbours = for y <- yIndex - 1 to yIndex + 1; x <- xIndex - 1 to xIndex + 1 yield (x, y)
     val bitString = neighbours
       .map(this.getValue.tupled)
@@ -23,18 +23,18 @@ class Grid(g: IndexedSeq[String], defaultChar: Char = '.'):
       .mkString
     Integer.parseInt(bitString, 2)
 
-  def getValue(x: Int, y: Int): Char =
+  private def getValue(x: Int, y: Int): Char =
     if x >= 0 && x < g.head.length && y >= 0 && y < g.size then g(y)(x)
-    else '.'
+    else defaultChar
 
-  def applyAlgorythm(algo: String): Grid =
-    val newImage = (-2 to g.size + 1).map { y =>
-      (-2 to g.head.length + 1).map { x =>
+  def applyAlgorithm(algo: String): Grid =
+    val newImage = (-1 to g.size).map { y =>
+      (-1 to g.head.length).map { x =>
         algo(getBitValue(x, y))
-      }.mkString
+      }
     }
-    val defaultChar = algo(0)
-    Grid(newImage, defaultChar)
+    val newDefault = if defaultChar == '#' then algo.last else algo.head
+    Grid(newImage, newDefault)
 
   def countLightPixels: Int = g.flatten.count(_ == '#')
 end Grid
@@ -42,15 +42,20 @@ end Grid
 def exercise1(source: Source): Int =
   val (algorithm, image) = source.readAlgorithmAndImage
 
-  val appliedOnce  = image.applyAlgorythm(algorithm)
-  val appliedTwice = appliedOnce.applyAlgorythm(algorithm)
+  val appliedOnce  = image.applyAlgorithm(algorithm)
+  val appliedTwice = appliedOnce.applyAlgorithm(algorithm)
 
   appliedTwice.countLightPixels
+
+def exercise2(source: Source): Int =
+  val (algo, image) = source.readAlgorithmAndImage
+
+  (0 until 50).foldLeft(image)((i, _) => i.applyAlgorithm(algo)).countLightPixels
 
 private lazy val input: Source = Source.fromResource("exerciseInputs/input_d20.txt")
 
 @main
 def run_20_1(): Unit = printWithRuntime(exercise1(input))
 
-//@main
-//def run_18_2(): Unit = printWithRuntime(exercise2(input))
+@main
+def run_20_2(): Unit = printWithRuntime(exercise2(input))
--- src/test/scala/eu/derfniw/aoc2021/d20/Day20Test.scala
+++ src/test/scala/eu/derfniw/aoc2021/d20/Day20Test.scala
@@ -7,5 +7,8 @@ class Day20Test extends munit.FunSuite:
   test("Part 1 works per example") {
     assertEquals(exercise1(Source.fromResource("testin_d20.txt")), 35)
   }
+  test("Part 2 works per example") {
+    assertEquals(exercise2(Source.fromResource("testin_d20.txt")), 3351)
+  }
 
 end Day20Test
